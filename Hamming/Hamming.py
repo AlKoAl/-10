c64d69c8c56564638882b28daf53d80c9cae4ee1
AlKoAl <32274055+AlKoAl@users.noreply.github.com>
2018-03-09 08:20:53 +0300

    Update Hamming.py

--- Hamming/Hamming.py
+++ Hamming/Hamming.py
@@ -1,4 +1,4 @@
-string = input('Введите строку из 0 и 1')
+string = input('Введите строку из 0 и 1')   # Вводим последовательность 0 и 1
 k = 0
 while True:
     if k <= 2**k - len(string) - 1:
@@ -7,10 +7,10 @@ while True:
         k += 1
 string = ' ' + string[:]
 for i in range(1, k):
-    string = string[: (2 ** i) - 1] + ' ' + string[(2 ** i) - 1:]
+    string = string[: (2 ** i) - 1] + ' ' + string[(2 ** i) - 1:]  # ставим пропуски на номера со степенями 2-ки
 
 
-def part_of_coding(digits):
+def part_of_coding(digits):   # поставит на пропуски цифры
     def func1(s):
         nonlocal score, digits
         while s * 2 ** i <= len(digits):
@@ -52,8 +52,8 @@ print("Закодированная последовательность:", stri
 error = input("Введите изменённую последовательность с одной ошибкой:")
 error = error[2:]
 for i in range(k - 1, 1, -1):
-    error = error[:(2**i - 3)] + error[(2**i - 2):]
-while True:
+    error = error[:(2**i - 3)] + error[(2**i - 2):]   # Вырезали кодирующие биты
+ while True:
     if k <= 2**k - len(error) - 1:
         break
     else:
@@ -61,10 +61,10 @@ while True:
 error = ' ' + error[:]
 for i in range(1, k):
     error = error[: (2 ** i) - 1] + ' ' + error[(2 ** i) - 1:]
-error = part_of_coding(error)
+error = part_of_coding(error)    # снова пропуски заполняем цифрам
 print("Переприсвоение кодирующих бит:", error)
 i = 0
-numbers = []
+numbers = []   # Совпадение/несовпадение кодирующих битов
 while 2**i <= len(string):
     if error[2**i-1] == string[2**i-1]:
         numbers.append(0)
@@ -76,7 +76,7 @@ sum = 0
 for i in range(len(numbers)):
     sum += 2**i*numbers[i]
 print("Ошибочный бит:", sum)
-if sum == 0:
+if sum == 0:  # Изменяем ошибочный бит или не изменяем ничего
     error = error
 else:
     if error[sum - 1] == '1':
@@ -85,5 +85,5 @@ else:
         error = error[: sum - 1] + '1' + error[sum:]
 error = error[2:]
 for i in range(k - 1, 1, -1):
-    error = error[:(2**i - 3)] + error[(2**i - 2):]
+    error = error[:(2**i - 3)] + error[(2**i - 2):]   # снова убираем кодирующие биты
 print(error)
